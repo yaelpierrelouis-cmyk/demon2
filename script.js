8a71e4da138b04d75bb9bbfb10e894780a9503f3
yaelpierrelouis-cmyk <yael.pierrelouis@umail.utm.ac.mu>
2026-01-23 11:43:02 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function toggleMenu() {
+  document.getElementById("nav-menu").classList.toggle("active");
+}
+
+window.addEventListener("scroll", () => {
+  document.querySelectorAll(".reveal").forEach(el => {
+    if (el.getBoundingClientRect().top < window.innerHeight - 100) {
+      el.classList.add("active");
+    }
+  });
+});
